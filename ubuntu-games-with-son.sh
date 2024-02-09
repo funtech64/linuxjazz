6ac5ba6e53a29fc1793deef2bc9bf916b785640a
Jonathan Jolly <63826499+funtech64@users.noreply.github.com>
2024-02-08 17:51:45 -0700

    Add files via upload

---
+++ ubuntu-games-with-son.sh
@@ -0,0 +1,11 @@
+/bin/sh
+
+apt-get install ubuntu-edu-primary ubuntu-edu-secondary bomber bovo granatier kapman katomic kblackbox kblocks kbounce kbreakout kdiamond kfourinline kgoldrunner kigo killbots kiriki kjumpingcube klickety klines kmahjongg kmines knavalbattle knetwalk kolf kollision konquest kpat kreversi kshisen ksirk ksnakeduel kspaceduel ksquares ksudoku ktuberling kubrick lskat palapeli picmi kajongg -y --install-recommends
+
+sudo apt-get install kdeedu
+
+
+apt install gnome-sudoku dreamchess steam:i386 gnome-mahjongg
+
+apt install stellarium google-earth-pro-stable -y --install-recommends
+
