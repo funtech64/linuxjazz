9089442a05dfe22e2e3c52384f8974da390c4e6f
Jonathan Jolly <63826499+funtech64@users.noreply.github.com>
2024-02-08 17:57:17 -0700

    Update ubuntu-games-with-son.sh

--- ubuntu-games-with-son.sh
+++ ubuntu-games-with-son.sh
@@ -1,4 +1,4 @@
-/bin/sh
+#!/bin/sh
 
 apt-get install ubuntu-edu-primary ubuntu-edu-secondary bomber bovo granatier kapman katomic kblackbox kblocks kbounce kbreakout kdiamond kfourinline kgoldrunner kigo killbots kiriki kjumpingcube klickety klines kmahjongg kmines knavalbattle knetwalk kolf kollision konquest kpat kreversi kshisen ksirk ksnakeduel kspaceduel ksquares ksudoku ktuberling kubrick lskat palapeli picmi kajongg -y --install-recommends
 
